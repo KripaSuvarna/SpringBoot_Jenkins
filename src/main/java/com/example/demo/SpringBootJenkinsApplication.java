b1961f9f133d655ce52a8bfb30409a3af3aa42e2
KripaSuvarna <kripasuvarna@gmail.com>
2019-01-09 11:48:06 -0500

    first commit of SpringBoot-Jenkin project

---
+++ src/main/java/com/example/demo/SpringBootJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsApplication {
+
+	static Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started...");
+	}
+	
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/demo/SpringBootJenkinsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJenkinsApplicationTests {
+
+	Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplicationTests.class);
+	
+	@Test
+	public void contextLoads() {
+		logger.info("test case executing.....");
+		assertEquals(true, true);
+	}
+
+}
+
